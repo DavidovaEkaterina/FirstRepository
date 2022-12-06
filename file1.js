a0f898fb1f62f8dcfd7ea0ccba23b72bbc2703e3
Ekaterina Davidova <davkat181611@gmail.com>
2022-12-06 19:54:37 +0300

    yrtyr

--- file1.js
+++ file1.js
@@ -2,9 +2,9 @@ let a = 5;
 let b = 1;
 let c = 6;
 let j = "line from user 2";
-"change file 1 for test merge";
-"Good morning" ("from branch main");
+("change file 1 for test merge");
+"Good morning"("from branch main");
 
-"line from user1 lesson about rebase";
-456
-
+("line from user1 lesson about rebase");
+456;
+let f = 5;
